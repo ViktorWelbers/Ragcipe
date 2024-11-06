47e9d0c492447d004532dd4525699c1947c9606c
Viktor Linux <v.welbers@gmx.de>
2024-11-06 21:33:58 +0000

    add embedding client stuff

--- db/embeddings.go
+++ db/embeddings.go
@@ -10,24 +10,37 @@ type Qdrant struct {
 	client *qdrant.Client
 }
 
-func NewClient() (Qdrant, error) {
+func NewClient() (*Qdrant, error) {
 	client, err := qdrant.NewClient(&qdrant.Config{
 		Host: "localhost",
 		Port: 6334,
 	})
 	if err != nil {
-		return Qdrant{}, err
+		return &Qdrant{}, err
 	}
 
-	return Qdrant{client: client}, nil
+	return &Qdrant{client: client}, nil
 }
 
 func (q *Qdrant) CreateCollection() {
 	q.client.CreateCollection(context.Background(), &qdrant.CreateCollection{
 		CollectionName: "Recipes",
 		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
-			Size:     4,
+			Size:     1024,
 			Distance: qdrant.Distance_Cosine,
 		}),
+	})
+}
+
+func (q *Qdrant) InsertVector(embedding []float64) {
+	operationInfo, err := q.client.Upsert(context.Background(), &qdrant.UpsertPoints{
+		CollectionName: "Recipes",
+		Points: []*qdrant.PointStruct{
+			{
+				Id:      qdrant.NewIDNum(1),
+				Vectors: qdrant.NewVectors(float32(embedding)),
+				Payload: qdrant.NewValueMap(map[string]any{"city": "London"}),
+			},
+		},
 	})
 }
