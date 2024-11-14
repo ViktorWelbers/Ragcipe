c008a6c10378e6b20da9b244d05683c256350fbc
Viktor Linux <v.welbers@gmx.de>
2024-11-14 19:00:16 +0000

    update

--- db/embeddings.go
+++ db/embeddings.go
@@ -32,15 +32,38 @@ func (q *Qdrant) CreateCollection() {
 	})
 }
 
-func (q *Qdrant) InsertVector(embedding []float64) {
+func (q *Qdrant) InsertVector(embedding []float64, embeddingData map[string]any) (*qdrant.UpdateResult, error) {
+	embeddingVectors := make([]float32, len(embedding))
+	for i, v := range embedding {
+		embeddingVectors[i] = float32(v)
+	}
 	operationInfo, err := q.client.Upsert(context.Background(), &qdrant.UpsertPoints{
 		CollectionName: "Recipes",
 		Points: []*qdrant.PointStruct{
 			{
 				Id:      qdrant.NewIDNum(1),
-				Vectors: qdrant.NewVectors(float32(embedding)),
-				Payload: qdrant.NewValueMap(map[string]any{"city": "London"}),
+				Vectors: qdrant.NewVectors(embeddingVectors...),
+				Payload: qdrant.NewValueMap(embeddingData),
 			},
 		},
 	})
+	return operationInfo, err
+}
+
+func (q *Qdrant) QueryVector(embedding []float64) (*qdrant.UpdateResult, error) {
+	embeddingVectors := make([]float32, len(embedding))
+	for i, v := range embedding {
+		embeddingVectors[i] = float32(v)
+	}
+	operationInfo, err := q.client.QueryBatch(context.Background(), &qdrant.QueryBatchPoints{
+		CollectionName: "Recipes",
+		QueryPoints: []*qdrant.QueryPoints{
+			{
+				Id:      qdrant.NewIDNum(1),
+				Vectors: qdrant.NewVectors(embeddingVectors...),
+				Payload: qdrant.NewValueMap(embeddingData),
+			},
+		},
+	})
+	return operationInfo, err
 }
